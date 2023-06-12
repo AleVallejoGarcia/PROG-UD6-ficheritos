1c510af65cdd1642229560e20aa2d181bfeadf0c
AleVallejoGarcia <usr2075@salesianos-lacuesta.net>
2023-06-12 12:53:06 +0100

    fix(MainSegundaActividad): se cambia el bucle while por un for i

--- src/actividades/segundaActividad/MainSegundaActividad.java
+++ src/actividades/segundaActividad/MainSegundaActividad.java
@@ -5,14 +5,12 @@ public class MainSegundaActividad {
     public static void main(String[] args) throws Exception {
         FileReader reader = new FileReader("src/ficheros/actividad2.txt");
         BufferedReader br = new BufferedReader(reader);
-        int conteo = 0;
         String textoFichero = br.readLine();
         String textoFormateado = "";
 
     
-        while (conteo < textoFichero.length()) {
-            textoFormateado += textoFichero.charAt(conteo) + "_" + (int) textoFichero.charAt(conteo) + ",";
-            conteo++;
+        for (int i = 0; i < textoFichero.length(); i++) {
+            textoFormateado += textoFichero.charAt(i) + "_" + (int) textoFichero.charAt(i) + ",";
         }
 
         reader.close();
